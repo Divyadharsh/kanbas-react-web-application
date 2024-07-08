eda5be27946c5ad932b329d2f6a592908f927432
Divyadharsh <divyadharshini0410@gmail.com>
2024-07-08 18:16:26 -0400

    First commit to a1

---
+++ src/Labs/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+
+function App() {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <Lab1 />
+      <Lab2 />
+      <Lab3 />
+    </div>
+  );
+}
+export default App;
