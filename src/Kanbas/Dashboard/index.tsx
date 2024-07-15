283cd1b27dd0d9a9a093fd4339fdc85605e41a83
Divyadharsh <divyadharshini0410@gmail.com>
2024-07-15 10:25:51 -0400

    Course file Dashboard Updation

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
                                   <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack software developer</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack 202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234.2023.12971 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -34,15 +34,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1235 Nodejs
+                                  CS2345 Angular JS
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Web Stack Validation </a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS2345.2023.19865 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -56,15 +54,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1236 Bootstarp
+                                  CS8910 TypeScript
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Type Script Validation</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS8910.2023.10865 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -78,15 +74,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1237 Moogose
+                                  CS3456 Core Java
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack JAVA developer</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS3456.2023.97342 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -100,15 +94,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1238 DataBase
+                                  CS6120 Natural Language Processing
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Machine Learning Algorithms</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS6120.2023.21890 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -122,15 +114,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1239 JSON
+                                  CS6704 Python Programming
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Python Developer</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS6704.2023.86501 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
@@ -144,37 +134,13 @@ export default function Dashboard() {
                               <a className="wd-dashboard-course-link"
                                  href="#/Kanbas/Courses/1234/Home"
                                  style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1240 Golang
+                                  CS6540 Programming and Design Principle
                               </a>
                               <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Programming Algorithms</a>
                               </p>
                               <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
-                              </p>
-                              <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div className="wd-dashboard-course col" style={{width: "300px"}}>
-                      <div className="card">
-                          <img src="/images/reactjs.jpg" className="card-img-top" alt="bg img"/>
-                          <div className="card-body">
-                              <a className="wd-dashboard-course-link"
-                                 href="#/Kanbas/Courses/1234/Home"
-                                 style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                                  CS1241 Python
-                              </a>
-                              <p className="wd-dashboard-course-title card-text">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      software developer</a>
-                              </p>
-                              <p className="wd-dashboard-course-title course-info">
-                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">Full Stack
-                                      202440_2 Summer 1 2024 Semester Full Term Grad</a>
+                                  <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS6540.2023.40567 Fall_2024 Semester Full Term</a>
                               </p>
                               <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
                           </div>
