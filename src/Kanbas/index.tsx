24aabcd3d752d03d43563651a25fa702fb38940b
Divyadharsh <divyadharshini0410@gmail.com>
2024-07-27 15:08:33 -0400

    kanbas page fix

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -33,25 +33,31 @@ export default function Kanbas() {
   };
   return (
     <Provider store={store}>
-    <div id="wd-kanbas">
-      <KanbasNavigation />
-      <div className="wd-main-content-offset p-3">
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard" />} />
-          <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={
-            <Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>
-          } />
-          <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
-        </Routes>
+      <div id="wd-kanbas" className="h-100">
+        <div className="d-flex h-100">
+          <div className="d-none d-md-block bg-black">
+            <KanbasNavigation />
+          </div>
+          <div className="flex-fill p-4">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Dashboard" element={
+                <Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}/>
+              } />
+              <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
     </Provider>
     );
   }
