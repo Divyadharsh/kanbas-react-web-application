9cce6728ea3bb74a2ab7687dcc0d34898514eae5
Divyadharsh <divyadharshini0410@gmail.com>
2024-08-05 22:59:42 -0400

    commiting to the a5-First Push

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,169 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { addAssignment, updateAssignment } from "./reducer";
+
+export default function AssignmentEditor() {
+  const { cid, aid } = useParams();
+  const { assignments } = useSelector((state: any) => state.assignmentsReducer);
+  const dispatch = useDispatch();
+  let assignment = {
+    title: "New Assignment",
+    description: "New Assignment Description",
+    points: 100,
+    due_date: "2024-01-01",
+    available_from: "2024-01-01",
+    available_until: "2024-01-01",
+    course: cid,
+  };
+  if (aid !== "new") {
+    assignment = assignments.find((a: any) => a._id === aid);
+  }
+  const [assignmentName, setAssignmentName] = useState(assignment.title)
+  const [assignmentDescription, setAssignmentDescription] = useState(assignment.description)
+  const [assignmentPoints, setAssignmentPoints] = useState(assignment.points)
+  const [assignmentDueDate, setAssignmentDueDate] = useState(assignment.due_date)
+  const [assignmentAvailableFrom, setAssignmentAvailableFrom] = useState(assignment.available_from)
+  const [assignmentAvailableUntil, setAssignmentAvailableUntil] = useState(assignment.available_until)
+  return (
+    <div id="wd-assignments-editor">
+      <div className="container">
+        <div className="row my-3">
+          <label htmlFor="wd-name" className="form-label">Assignment Name</label>
+          <input id="wd-name" type="text" className="form-control" value={assignmentName} onChange={e => setAssignmentName(e.target.value)} />
+        </div>
+        <div className="row my-3">
+          <textarea id="wd-description" className="form-control" rows={10} onChange={e => setAssignmentDescription(e.target.value)}>{assignmentDescription}</textarea>
+        </div>
+        <div className="row my-3">
+          <div className="col-3">
+            <label htmlFor="wd-points" className="col-form-label float-end">Points</label>
+          </div>
+          <div className="col">
+            <input id="wd-points" type="number" className="form-control" value={assignmentPoints} onChange={e => setAssignmentPoints(+e.target.value)} />
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-3">
+            <label htmlFor="wd-group" className="col-form-label float-end">Assignment Group</label>
+          </div>
+          <div className="col">
+            <select id="wd-group" className="form-select">
+              <option selected value="assignments">
+                  ASSIGNMENTS</option>
+              <option value="quizzes">
+                  QUIZZES</option>
+              <option value="exams">
+                  EXAMS</option>
+              <option value="project">
+                  PROJECT</option>
+            </select>
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-3">
+            <label htmlFor="wd-display-grade-as" className="col-form-label float-end">Display Grade as</label>
+          </div>
+          <div className="col">
+            <select id="wd-display-grade-as" className="form-select">
+              <option selected value="percentage">Percentage</option>
+            </select>
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-3">
+            <label htmlFor="wd-submission-type" className="col-form-label float-end">Submission Type</label>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <select id="wd-submission-type" className="form-select">
+                    <option selected value="online">Online</option>
+                  </select>
+                </div>
+                <div className="row mt-4">
+                  <label><b>Online Entry Options: </b></label>
+                </div>
+                <div className="row my-2">
+                  <div className="form-check my-2">
+                    <input type="checkbox" name="text-entry" id="wd-text-entry" className="form-check-input" />
+                    <label htmlFor="wd-text-entry" className="form-check-label">Text Entry</label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input type="checkbox" name="website-url" id="wd-website-url" className="form-check-input" />
+                    <label htmlFor="wd-website-url" className="form-check-label">Website URL</label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input type="checkbox" name="media-recordings" id="wd-media-recordings" className="form-check-input" />
+                    <label htmlFor="wd-media-recordings" className="form-check-label">Media Recordings</label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input type="checkbox" name="student-annotation" id="wd-student-annotation" className="form-check-input" />
+                    <label htmlFor="wd-student-annotation" className="form-check-label">Student Annotation</label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input type="checkbox" name="file-upload" id="wd-file-upload" className="form-check-input" />
+                    <label htmlFor="wd-file-upload" className="form-check-label">File Uploads</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-3">
+            <label htmlFor="wd-assign" className="col-form-label float-end">Assign</label>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <label htmlFor="wd-assign-to" className="form-label"><b>Assign to</b></label>
+                  <div className="input-group">
+                    <button className="btn btn-outline-secondary">Every one</button>
+                    <input id="wd-assign-to" type="text" className="form-control" />
+                  </div>
+                </div>
+                <div className="row mt-4">
+                  <label htmlFor="wd-due-date"><b>Due</b></label>
+                  <input id="wd-due-date" type="datetime-local" className="form-control" value={assignmentDueDate} onChange={e => setAssignmentDueDate(e.target.value)} />
+                </div>
+                <div className="row my-2">
+                  <div className="col">
+                    <label htmlFor="wd-available-from"><b>Available from</b></label>
+                    <input id="wd-available-from" type="datetime-local" className="form-control" value={assignmentAvailableFrom} onChange={e => setAssignmentAvailableFrom(e.target.value)} />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="wd-available-until"><b>Until</b></label>
+                    <input id="wd-available-until" type="datetime-local" className="form-control" value={assignmentAvailableUntil} onChange={e => setAssignmentAvailableUntil(e.target.value)} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <hr />
+        </div>
+        <Link key="save" to={`/Kanbas/Courses/${cid}/Assignments/`} className="btn btn-danger float-end ms-2" onClick={() => {
+          let lastestAssignment = {
+            ...assignment,
+            title: assignmentName,
+            description: assignmentDescription,
+            points: assignmentPoints,
+            due_date: assignmentDueDate,
+            available_from: assignmentAvailableFrom,
+            available_until: assignmentAvailableUntil,
+          };
+          if (aid !== "new") {
+            dispatch(updateAssignment(lastestAssignment))
+          } else {
+            dispatch(addAssignment(lastestAssignment))
+          }
+        }}>Save</Link>
+        <Link key="cancel" to={`/Kanbas/Courses/${cid}/Assignments/`} className="btn btn-secondary float-end">Cancel</Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { BsGripVertical } from "react-icons/bs"
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FaPlus } from "react-icons/fa6";
+import { IoEllipsisVertical } from "react-icons/io5";
+import { FaRegEdit } from "react-icons/fa";
+import AssignmentsControls from "./AssignmentControls";
+import AssignmentControlButtons from "./AssignmentControlButtons";
+import { deleteAssignment } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
+export default function Assignments() {
+  const { cid } = useParams();
+  const { assignments } = useSelector((state: any) => state.assignmentsReducer);
+  const dispatch = useDispatch();
+  return (
+    <div id="wd-assignments">
+      <AssignmentsControls cid={cid!}/>
+      <ul id="wd-assignments" className="list-group rounded-0 my-2">
+        <div className="wd-title p-3 ps-2 bg-secondary">
+          <BsGripVertical className="fs-4" />
+          <IoMdArrowDropdown className="mx-2" />
+          <b>ASSIGNMENTS</b>
+          <IoEllipsisVertical className="float-end mt-1"/>
+          <FaPlus className="float-end text-secondary mx-2 mt-1" />
+          <span className="badge rounded-pill float-end text-dark border border-secondary">40% of Total</span>
+        </div>
+        <ul id="wd-assignment-list" className="list-group rounded-0">
+          {assignments.filter((assignment: any) => assignment.course === cid).map((assignment: any) => (
+            <li className="wd-assignment-list-item list-group-item p-3" style={{borderLeft: "5px solid green"}}>
+              <div className="row align-items-center">
+                <div className="col-1">
+                  <BsGripVertical className="fs-4" />
+                </div>
+                <div className="col-1">
+                  <FaRegEdit className="text-success" />
+                </div>
+                <div className="col">
+                  <Link className="wd-assignment-link text-dark link-underline link-underline-opacity-0"
+                    key={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
+                    to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
+                    <h5><b>{assignment._id} {assignment.title}</b></h5>
+                  </Link>
+                  <p><span className="text-danger">Multiple Modules</span> | <b>Due</b> {assignment.avialble_assignment} at 11:59pm | {assignment.points} pts</p>
+                </div>
+                <div className="col float-end">
+                  <AssignmentControlButtons assignmentId={assignment._id} 
+                  deleteAssignment={assignmentId => dispatch(deleteAssignment(assignmentId))} />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ul>
+    </div>
+  );
+}
