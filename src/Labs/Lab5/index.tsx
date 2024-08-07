c4289307ea38de930302486632e8956782829166
Divyadharsh <divyadharshini0410@gmail.com>
2024-08-06 22:50:38 -0400

    Adding Lab5 component Arrays and Objects

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,27 +1,29 @@
-import React from "react";
-import EnvironmentVariables from "./EnvironmentVariables";
-import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
-import WorkingWithObjects from "./WorkingWithObjects";
+import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
+import PathParameters from "./PathParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href={`${REMOTE_SERVER}/lab5/welcome`}
-           className="list-group-item">
-           Welcome
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+          Welcome
         </a>
-      </div><hr/>
+      </div><hr />
       <EnvironmentVariables />
-      <PathParameters />
-      <QueryParameters />
-      <WorkingWithObjects />
-      <WorkingWithArrays />
+      <PathParameters/>
+      <QueryParameters/>
+      <WorkingWithObjects/>
+      <WorkingWithArrays/>
+      <HttpClient/>
+      <WorkingWithObjectsAsynchronously/>
+      <WorkingWithArraysAsynchronously/>
     </div>
   );
 }
-
-  
