8f33282904032bc904b68e8e976170833cc793f1
Dante LoPriore <dante.lopriore@gmail.com>
2024-08-11 12:57:46 -0400

    Commiting first push to branch a6

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,98 @@
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./style.css";
+import { useState, useEffect } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import * as client from "./Courses/client";
+
+interface Course {
+  _id: string;
+  name: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+  description: string;
+}
+
+export default function Kanbas() {
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [course, setCourse] = useState<Course>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.webp",
+    description: "New Description",
+  });
+
+  const fetchCourses = async () => {
+    const fetchedCourses = await client.fetchAllCourses();
+    setCourses(fetchedCourses);
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const addNewCourse = async () => {
+    const newCourse = await client.createCourse(course);
+    setCourses([ ...courses, newCourse ]);
+  };
+  const deleteCourse = async (courseId: string) => {
+    await client.deleteCourse(courseId);
+    setCourses(courses.filter(
+      (c) => c._id !== courseId));
+  };
+
+  const updateCourse = async () => {
+    await client.updateCourse(course);
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+  return (
+    <Provider store={store}>
+    <div id="wd-kanbas" className="h-100">
+      <div className="d-flex h-100">
+        <div className="d-none d-md-block bg-black">
+          <KanbasNavigation />
+        </div>
+        <div className="flex-fill p-4">
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
+            <Route
+              path="Dashboard"
+              element={
+                <Dashboard
+                  courses={courses}
+                  course={course}
+                  setCourse={setCourse}
+                  addNewCourse={addNewCourse}
+                  deleteCourse={deleteCourse}
+                  updateCourse={updateCourse}
+                />
+              }
+            />
+            <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+            <Route path="Inbox" element={<h1>Inbox</h1>} />
+          </Routes>
+        </div>
+      </div>
+      </div>
+    </Provider>
+  );
+}
