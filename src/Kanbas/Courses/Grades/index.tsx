982cfd67837042a83761e29f7ff5c23b6176b975
Divyadharshini Muruganandham <divyadharshini0410@gmail.com>
2024-08-12 17:18:07 -0400

    Adding database files to a6

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -1,150 +1,74 @@
-import { FaFileImport, FaFileExport, FaSearch } from 'react-icons/fa'
-import { IoMdSettings } from 'react-icons/io'
-import { MdKeyboardArrowDown } from 'react-icons/md'
-import { BiFilterAlt } from 'react-icons/bi'
-import { LiaFileExportSolid } from 'react-icons/lia'
-import { useParams } from 'react-router-dom'
-import * as db from '../../Database'
+import React from "react";
+import { FaFileImport, FaFileExport, FaCog } from "react-icons/fa";
+import { CiFilter } from "react-icons/ci";
+import { useParams } from "react-router";
+import { assignments, enrollments, users, grades } from "../../Database";
 
-export default function Grades () {
-  const { cid } = useParams()
-  const courseAssignments = db.assignments.filter(a => a.course === cid)
-  const courseEnrollments = db.enrollments.filter(e => e.course === cid)
-  const courseUsers = courseEnrollments.map(e =>
-    db.users.find(u => u._id === e.user)
-  )
-  const courseGrades = db.grades.filter(g =>
-    courseAssignments.some(a => a._id === g.assignment)
-  )
-
+export default function Grades() {
+  const { cid } = useParams();
+  const courseAssignments = assignments.filter(a => a.course === cid);
+  const courseEnrollments = enrollments.filter(e => e.course === cid);
   return (
-    <div className='container mt-5'>
-      <div className='d-flex justify-content-between align-items-center mb-3'>
-        <div className='d-flex flex-column'>
-          <label htmlFor='search-students' className='form-label'>
-            Student Names
-          </label>
-          <div className='input-group'>
-            <span className='input-group-text bg-white border-end-0'>
-              <FaSearch />
-            </span>
-            <input
-              id='search-students'
-              className='form-control'
-              type='text'
-              placeholder='Search Students'
-            />
-            <button className='btn btn-light me-2'>
-              <MdKeyboardArrowDown />
-            </button>
-          </div>
+    <div className="container">
+      <button className="btn btn-secondary float-end">
+        <FaCog className="me-2 fs-5" />
+      </button>
+      <button className="btn btn-secondary float-end mx-2 dropdown-toggle">
+        <FaFileExport className="me-2 fs-5" />
+        Export
+      </button>
+      <button className="btn btn-secondary float-end">
+        <FaFileImport className="me-2 fs-5" />
+        Import
+      </button>
+      <br />
+      <div className="row mt-4">
+        <div className="col">
+          <label htmlFor="wd-student-names" className="form-label"><b>Student Names</b></label>
+          <select id="wd-student-names" className="form-select" style={{backgroundImage: "url(https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg)", backgroundSize: "13px", backgroundRepeat: "no-repeat", backgroundPosition: "10px center", "paddingLeft": "30px"}}></select>
         </div>
-        <div className='d-flex flex-column'>
-          <label htmlFor='search-assignments' className='form-label'>
-            Assignment Names
-          </label>
-          <div className='input-group'>
-            <span className='input-group-text bg-white border-end-0'>
-              <FaSearch />
-            </span>
-
-            <input
-              id='search-assignments'
-              className='form-control'
-              type='text'
-              placeholder='Search Assignments'
-            />
-            <button className='btn btn-light me-2'>
-              <MdKeyboardArrowDown />
-            </button>
-          </div>
-        </div>
-        <div className='d-flex'>
-          <button className='btn btn-light me-2'>
-            <FaFileImport /> Import
-          </button>
-          <div className='btn-group'>
-            <button
-              className='btn btn-light me-2 dropdown-toggle'
-              data-bs-toggle='dropdown'
-              aria-expanded='false'
-            >
-              <FaFileExport /> Export
-            </button>
-            <ul className='dropdown-menu'>
-              <li>
-                <a className='dropdown-item' href='#'>
-                  Export Option 1
-                </a>
-              </li>
-              <li>
-                <a className='dropdown-item' href='#'>
-                  Export Option 2
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className='btn-group'>
-            <button className='btn btn-light me-2'>
-              <IoMdSettings />
-            </button>
-
-            <ul className='dropdown-menu'>
-              <li>
-                <a className='dropdown-item' href='#'>
-                  Export Option 1
-                </a>
-              </li>
-              <li>
-                <a className='dropdown-item' href='#'>
-                  Export Option 2
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="col">
+          <label htmlFor="wd-assignment-names" className="form-label"><b>Assignment Names</b></label>
+          <select id="wd-assignment-names" className="form-select" style={{backgroundImage: "url(https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg)", backgroundSize: "13px", backgroundRepeat: "no-repeat", backgroundPosition: "10px center", "paddingLeft": "30px"}}></select>
         </div>
       </div>
-
-      <div>
-        <button
-          className='btn btn-light me-2'>
-          <BiFilterAlt />
-          Apply Filter
+      <div className="row my-2 row-cols-auto">
+        <button className="btn btn-secondary">
+          <CiFilter className="me-2" />
+          Apply Filters
         </button>
       </div>
-
-      <div className='table-responsive'>
-        <table className='table table-striped'>
-          <thead>
-            <tr className='table-light'>
-              <th>Student Name</th>
-              {courseAssignments.map(a => (
-                <th key={a._id}>
-                  <br /> Out of 100
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {courseUsers.map(user => (
-              <tr key={user?._id}>
-                <td className='text-danger'>
-                  {user?.firstName} {user?.lastName}
-                </td>
-                {courseAssignments.map(a => {
-                  const grade = courseGrades.find(
-                    g => g.student === user?._id && g.assignment === a._id
-                  )
-                  return (
-                    <td key={a._id}>{grade ? `${grade.grade}%` : 'N/A'}</td>
-                  )
-                })}
+      <div className="row my-2">
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered align-middle">
+            <thead>
+              <tr>
+                <th scope="col" className="fs-6 fw-normal" style={{width: "20%"}}>Student Name</th>
+                {courseAssignments.map((assignment: any) => (
+                  <th scope="col" className="fs-6 fw-normal text-center" style={{width: "20%"}}>
+                    {assignment._id} - {assignment.title}<br />
+                    Out of {assignment.points}
+                  </th>
+                ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {courseEnrollments.map(enrollment => {
+                let user = users.find((u: any) => u._id === enrollment.user) as any
+                return (
+                  <tr>
+                    <th scope="row" className="text-danger">{user?.firstName} {user?.lastName}</th>
+                    {courseAssignments.map((assignment: any) => {
+                      let grade = grades.find(g => g.student === user?._id && g.assignment === assignment._id)
+                      return (<td className="text-center">{grade?.grade}</td>);
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
+  );
 }
